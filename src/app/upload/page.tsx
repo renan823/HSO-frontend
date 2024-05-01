4d08cf4ce22233904272394c302c56c292944640
renan823 <renan.trofino@gmail.com>
2024-05-01 20:09:55 -0300

    feat: new dropzone

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -2,45 +2,54 @@
 
 import ServerRequest from "@/services/ServerRequest";
 import { UploadIcon } from "lucide-react";
-import { ChangeEvent, FormEvent, createRef, useState } from "react";
+import { FormEvent, createRef, useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import toast from "react-hot-toast";
 
 export default function Upload () {
 
-    const [file, setFile] = useState<any>();
+    const [file, setFile] = useState<File | any>(null);
     const fileRef = createRef<HTMLInputElement>();
+
+    const onDrop = useCallback((files: File[]) => {
+        if (!files || files.length === 0) {
+            return toast.error("Nenhum arquivo selecionado");
+        }
+
+        if (files[0].name.split(".").pop() !== "xlsx"){
+            return toast.error("Extensão incorreta");
+        }
+
+        console.log("oi");
+
+        setFile(files[0]);
+    }, [])
+
+    const dropzone = useDropzone({
+        onDrop
+    })
 
     async function handleSubmit (event: FormEvent) {
         event.preventDefault();
 
-        const payload = new FormData();
+        if (file) {
+            const payload = new FormData();
 
-        payload.set("file", file);
+            payload.set("file", file);
 
-        const request = new ServerRequest("post", "/files/save", payload, { 'Content-Type': 'multipart/form-data' });
+            const request = new ServerRequest("post", "/files/save", payload, { 'Content-Type': 'multipart/form-data' });
 
-        const response = await request.handle();
+            const response = await request.handle();
 
-        if (response.getStatus() === 201) {
-            clearField();
-            return toast.success("Arquivo adicionado");
+            if (response.getStatus() === 201) {
+                clearField();
+                return toast.success("Arquivo adicionado");
+            } else {
+                return toast.error(response.getData().message ?? "Algo deu errado");
+            }
         } else {
-            return toast.error(response.getData().message ?? "Algo deu errado");
-        }
-    }
-
-    function handleFileChange (event: ChangeEvent<HTMLInputElement>) {
-        const input = event.target;
-
-        if (!input.files || input.files.length === 0) {
             return toast.error("Nenhum arquivo selecionado");
         }
-
-        if (input.files[0].name.split(".").pop() !== "xlsx"){
-            return toast.error("Extensão incorreta");
-        }
-
-        setFile(input.files[0]);
     }
 
     function clearField () {
@@ -51,24 +60,36 @@ export default function Upload () {
     }
 
     return (
-        <div className="flex w-3/6 m-auto p-6 bg-slate-800 rounded-lg">
+        <div className="flex-col w-3/6 p-6 bg-slate-800 rounded-lg">
             <div>
                 <h1 className="text-3xl text-violet-700 font-bold">Faça upload do arquivo</h1>
-                <h3 className="text-xl text-white font-bold">Use apenas arquivos .xlsx (Excel/Planilha)</h3>
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="w-full flex">
-                            <label htmlFor="dropzone">
-                                <div>
-                                    <p><span>Clique para escolher</span> ou arraste aqui</p>
-                                </div>
-                            </label>
-                            <input type="file" onChange={handleFileChange} ref={fileRef} name="dropzone" className="hidden"/>
-                        </div>
-                    </form>
-                </div>
+                    <h3 className="text-xl text-white font-bold">Use apenas arquivos .xlsx (Excel/Planilha)</h3>
             </div>
-            <div></div>
+            <div {...dropzone.getRootProps()} className="flex p-10 m-5 cursor-pointer border-slate-500 border-4 border-dashed bg-gray-700 justify-center hover:border-gray-500 rounded-lg">
+                <label htmlFor="dropzone">
+                    {
+                        file ? 
+                            <div>
+                                <div className="bg-white rounded-md p-3">
+                                    <p className="text-violet-700 text-lg text-center font-bold">Arquivo: {file.name}</p>
+                                </div>
+                            </div>
+                        :
+                            <div>
+                                <div className="p-3 flex justify-center">
+                                    <UploadIcon color="white" size={50}/>
+                                </div>
+                                <p className="text-xl text-white font-bold text-center"><span className="text-violet-600">Clique para escolher</span> ou arraste aqui</p>
+                            </div>
+                    }
+                </label>
+                <input {...dropzone.getInputProps()} type="file" name="dropzone" className="hidden" ref={fileRef}/>
+            </div>
+            <div className="flex justify-end px-5">
+                <button onClick={handleSubmit} className="bg-violet-700 px-10 py-2 rounded-md shadow-md shadow-violet-900">
+                    <p className="text-lg text-white font-bold text-center">Enviar</p>
+                </button>
+            </div>
         </div>
     )
 }
